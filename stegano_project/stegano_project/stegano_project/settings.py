6ba27b5468f958a027e7c01ebba34b063dccf1d1
Merawa Yore Gamo <dundeemerawa@gmail.com>
2024-01-02 05:42:52 +0100

    Config vercel

--- stegano_project/stegano_project/stegano_project/settings.py
+++ stegano_project/stegano_project/stegano_project/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = '^vyb6#b*j!g6nvk*qtt^=5przr4z)dherogu&f*0c%m#lec%qu'
 DEBUG = True
 
 # ALLOWED_HOSTS = ['.vercel.app', '.now.sh']
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['127.0.0.1', '.vercel.app', '.now.sh']
 
 
 # Application definition
@@ -123,7 +123,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 # Add this line
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static'),]
+STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
 MEDIA_URL = '/media/'
